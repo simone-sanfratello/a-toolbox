216f96ef37946dfe2224a20d2bbd2dc49f5c18b7
Simone Sanfratello <simone@braceslab.com>
2019-11-01 06:03:58 +0100

    feat(file): fs.isFile and fs.isDirectory

--- src/fs.js
+++ src/fs.js
@@ -10,15 +10,7 @@ const fs = {}
  * replace deprecated fs.exists
  * @method tools.fs.exists
  * @param {string} path (filePath) file path
- * @return {Promise.<boolean>} true if file exists - and it's a file
- * https://nodejs.org/api/fs.html#fs_fs_exists_path_callback
- * @test.arrange async function(input, sandbox) {
- *   const fs = require('../../src/fs')
- *   return fs.touch('/tmp/file')
- * }
- * @test.case '/tmp/file' > true
- * @test.case '/tmp/none' > false
- * @test.case '/tmp' > false
+ * @return {Promise.<boolean>} true if file exists
  */
 fs.exists = function (path) {
   return new Promise(function (resolve) {
@@ -41,15 +33,6 @@ fs.exists = function (path) {
  * @param {string} path (filePath) file path
  * @param {number} [mode=0o666]
  * @return {Promise.<void>}
- * @test.case '/tmp/touch-me'
- * @test.case '/none' ! new tollo.Error({code: 'EACCES'})
- * @test.assert async (result, input, output, sandbox) => {
- *   if(result) {
- *     return result.code === output.code
- *   }
- *   const fs = require('../../src/fs')
- *   return fs.exists(input[0])
- * }
  */
 fs.touch = function (path, mode = 0o666) {
   return new Promise(function (resolve, reject) {
@@ -72,15 +55,8 @@ fs.touch = function (path, mode = 0o666) {
  * delete file, optionally in safe mode
  * @method tools.fs.unlink
  * @param {string} path (filePath) file path
- * @param {boolean} [safe=true] if safe do not throw exception
+ * @param {boolean} [safe=false] if safe do not throw exception
  * @return {Promise.<void>}
- * @test.arrange async function(input, sandbox) {
- *   const fs = require('../../src/fs')
- *   return fs.touch('/tmp/file')
- * }
- * @test.case '/tmp/file'
- * @test.case '/tmp/none', false ! new tollo.Error({code: 'ENOENT'})
- * @test.case '/tmp/none', true
  */
 fs.unlink = function (path, safe) {
   return new Promise(function (resolve, reject) {
@@ -90,6 +66,46 @@ fs.unlink = function (path, safe) {
         return
       }
       resolve()
+    })
+  })
+}
+
+/**
+ * check if `path` exists and is a file
+ * @param {string} path path
+ * @return {Promise.<boolean>} true if file exists and it's a file
+ */
+fs.isFile = function (path) {
+  return new Promise(function (resolve) {
+    nativeFs.stat(path, function (err, stats) {
+      if (err || !stats) {
+        resolve(false)
+        return
+      } else if (stats.isFile()) {
+        resolve(true)
+        return
+      }
+      resolve(false)
+    })
+  })
+}
+
+/**
+ * check if `path` exists and is a directory
+ * @param {string} path path
+ * @return {Promise.<boolean>} true if file exists and it's a directory
+ */
+fs.isDirectory = function (path) {
+  return new Promise(function (resolve) {
+    nativeFs.stat(path, function (err, stats) {
+      if (err || !stats) {
+        resolve(false)
+        return
+      } else if (stats.isDirectory()) {
+        resolve(true)
+        return
+      }
+      resolve(false)
     })
   })
 }
